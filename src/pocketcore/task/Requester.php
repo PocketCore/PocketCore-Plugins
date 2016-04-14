7be0168adb0878001bb1c09b63870822827a3868
PrimusLV <K.Drivnieks@inbox.lv>
2016-04-14 17:32:18 +0000

    Seperated plugins from main repository

---
+++ src/pocketcore/task/Requester.php
@@ -0,0 +1,19 @@
+<?php
+namespace pocketcore\task;
+
+use pocketcore\Bridge;
+
+use pocketmine\scheduler\AsyncTask;
+
+use pocketmine\Server;
+
+class Requester extends AsyncTask {
+    
+    public function __construct(){}
+    
+    public function onRun(){}
+    
+    public function onCompletion(Server $server){
+        
+    }
+}
